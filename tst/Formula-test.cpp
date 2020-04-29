36243e3d918cab70c32dd0f64480d72d4592f36b
seancool61 <seancool61@hotmail.com>
2020-04-29 17:24:47 +0800

    jenkins

--- tst/Formula-test.cpp
+++ tst/Formula-test.cpp
@@ -7,7 +7,11 @@ TEST(blaTest, test1) {
     //act
     //assert
     EXPECT_EQ (Formula::bla (0),  0);
+}
+TEST(blaTest, test2) {
     EXPECT_EQ (Formula::bla (10), 20);
+}
+TEST(blaTest, test3) {
     EXPECT_EQ (Formula::bla (50), 100);
 }
 
