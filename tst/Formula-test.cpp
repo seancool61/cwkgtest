4ea61060c84636120bad93717e32f53e57e19514
seancool61 <seancool61@hotmail.com>
2020-04-29 21:26:31 +0800

    pet

--- tst/Formula-test.cpp
+++ tst/Formula-test.cpp
@@ -8,12 +8,24 @@ TEST(blaTest, test1) {
     //assert
     EXPECT_EQ (Formula::bla (0),  0);
 }
+
 TEST(blaTest, test2) {
     EXPECT_EQ (Formula::bla (10), 20);
 }
+
 TEST(blaTest, test3) {
     EXPECT_EQ (Formula::bla (50), 100);
 }
+
+TEST(petTest, test1) {
+    //arrange
+    Dog dog1("poochie",1);
+    //act
+    dog1.make_sound();
+    //assert
+    EXPECT_EQ (Pet::count,  1);
+}
+
 
 #if 0
 TEST(blaTest, test2) {
