4d26c099194839b8cc3c9db2f158007ebe5fe5e6
seancool61 <seancool61@hotmail.com>
2020-04-29 21:38:49 +0800

    pet

--- tst/Formula-test.cpp
+++ tst/Formula-test.cpp
@@ -2,36 +2,46 @@
 #include "../src/main.h"
 #include "../src/formula/Formula.h"
 
-TEST(blaTest, test1) {
+TEST(blaTest, test1)
+{
     //arrange
     //act
     //assert
-    EXPECT_EQ (Formula::bla (0),  0);
+    EXPECT_EQ(Formula::bla(0), 0);
 }
 
-TEST(blaTest, test2) {
-    EXPECT_EQ (Formula::bla (10), 20);
+TEST(blaTest, test2)
+{
+    EXPECT_EQ(Formula::bla(10), 20);
 }
 
-TEST(blaTest, test3) {
-    EXPECT_EQ (Formula::bla (50), 100);
+TEST(blaTest, test3)
+{
+    EXPECT_EQ(Formula::bla(50), 100);
 }
 
-TEST(petTest, test1) {
+TEST(petTest, test1)
+{
     //arrange
-    Dog dog1("poochie",1);
-    Dog dog2("Angel",14);
-    Dog dog3("Toby",14);
-    Cat cat1("Melissa",1);
-    //act
-    dog1.make_sound();
+    Dog dog1("poochie", 1);
+    Dog dog2("Angel", 14);
+    Dog dog3("Toby", 14);
+    Cat cat1("Melissa", 1);
     //assert
-    EXPECT_EQ (Pet::count,  4);
+    EXPECT_EQ(Pet::count, 4);
 }
 
-
-#if 0
-TEST(blaTest, test2) {
-    EXPECT_EQ (response01(0),  "Bla: 0");
+TEST(petTest, test1)
+{
+    //arrange
+    Dog dog1("poochie", 1);
+    Dog dog2("Angel", 14);
+    Dog dog3("Toby", 14);
+    Cat cat1("Melissa", 1);
+    //act
+    std::ostringstream stream;
+    stream << dog1;
+    std::string str = stream.str();
+    //assert
+    EXPECT_EQ(str, "My pet:poochie");
 }
-#endif
