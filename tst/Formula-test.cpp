d17d2c8f38824455e1459ebfbf701126dedbe50c
seancool61 <seancool61@hotmail.com>
2020-04-29 21:43:47 +0800

    pet

--- tst/Formula-test.cpp
+++ tst/Formula-test.cpp
@@ -10,16 +10,6 @@ TEST(blaTest, test1)
     EXPECT_EQ(Formula::bla(0), 0);
 }
 
-TEST(blaTest, test2)
-{
-    EXPECT_EQ(Formula::bla(10), 20);
-}
-
-TEST(blaTest, test3)
-{
-    EXPECT_EQ(Formula::bla(50), 100);
-}
-
 TEST(petTest, test1)
 {
     //arrange
