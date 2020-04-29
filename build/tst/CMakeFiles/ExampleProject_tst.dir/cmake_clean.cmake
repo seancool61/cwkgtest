bbfc2c14acdad5b92ceb21b7944a74d198ead82d
seancool61 <seancool61@hotmail.com>
2020-04-29 17:56:54 +0800

    googletest

---
+++ build/tst/CMakeFiles/ExampleProject_tst.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleProject_tst.dir/Formula-test.cpp.o"
+  "CMakeFiles/ExampleProject_tst.dir/main.cpp.o"
+  "ExampleProject_tst.pdb"
+  "ExampleProject_tst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleProject_tst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
